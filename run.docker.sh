0daa8aeb513a5e314b31f5d8ddc0d2773718cf00
promethee <lunette.francois@gmail.com>
2021-11-22 17:38:42 +0100

    Update run.docker.sh

--- run.docker.sh
+++ run.docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name $(basename "$PWD") $USER/$(basename "$PWD"):$(arch)
+docker run -d --privileged -v /dev/i2c-1:/dev/i2c-1 -v /dev/spidev0.0:/dev/spidev0.0 --restart always --name $(basename "$PWD") $USER/$(basename "$PWD"):latest
